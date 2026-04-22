0096425cd1ba723fe4ee2116d5c9a1b9e2869891
David <David.aa.leren@gmail.com>
2026-04-22 13:14:01 +0200

    fix

--- src/config.ts
+++ src/config.ts
@@ -12,5 +12,5 @@ export const appConfig = {
   enableDebug: false, // Vis debug-informasjon i UI
   enableOnboarding: true, // Vis onboarding ved første besøk
   enableQrScanner: false, // Vis QR-skanner-knapp og -side
-  enableTourBuilder: false, // Vis "Lag tur" siden
+  enableTourBuilder: true, // Vis "Lag tur" siden
 };
